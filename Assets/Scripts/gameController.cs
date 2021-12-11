1606dd81ac701e12bef73de90c39053a8673a45e
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-12-10 17:30:21 -0700

    Added text for freeze
    ready for post processing work on the cold color

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -11,23 +11,51 @@ public class gameController : MonoBehaviour
     public GameObject jumpCounter;
     public AudioSource iceSound;
 
+    bool freezeTextUp = false;
+    bool freezeTextDown = false;
+    public Text freezeText;
+    public Color lightBlue;
+
     // Change text sixe to reflect how large the screen is
     void Start()
     {
         jumpCounter.GetComponent<Text>().fontSize = Screen.width / 100 * 12;
     }
-
-    // Check if jumpCount has been changed, if it has update the jump Counter on the HUD
+    
     void Update()
     {
+        // Check if jumpCount has been changed, if it has update the jump Counter on the HUD
         if (previousCount != jumpCount)
         {
             jumpCounter.GetComponent<Text>().text = jumpCount.ToString();
             if (previousCount > jumpCount) // if jumpCount decreases/ player freezes, play sound
             {
                 iceSound.Play();
+                freezeTextUp = true;
             }
             previousCount = jumpCount;
+        }
+
+        // go from transparent to opaque and back down for the frozen text
+        if (freezeTextUp)
+        {   
+            lightBlue.a = lightBlue.a + 0.9f * Time.deltaTime;
+            freezeText.color = lightBlue;
+            Debug.Log(freezeText.color.a);
+            if (freezeText.color.a >= 1)
+            {
+                freezeTextUp = false;
+                freezeTextDown = true;
+            }
+        }
+        if (freezeTextDown)
+        {
+            lightBlue.a = lightBlue.a - 0.9f * Time.deltaTime;
+            freezeText.color = lightBlue;
+            if (freezeText.color.a <= 0)
+            {
+                freezeTextDown = false;
+            }
         }
     }
 }
