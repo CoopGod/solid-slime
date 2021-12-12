0aba0ed904816ef5ce25d4bf816def80af79297d
Cooper Goddard <57197353+CoopGod@users.noreply.github.com>
2021-12-12 14:25:34 -0700

    Added sheen overtop of frozen slime

--- Assets/Scripts/frozenSlime.cs
+++ Assets/Scripts/frozenSlime.cs
@@ -7,11 +7,34 @@ public class frozenSlime : MonoBehaviour
     // Global Variables
     public GameObject playerPrefab;
     GameObject spawnLocation;
+    Animator animator;
+    int triggerTime;
+    bool triggerSet = false;
+    float currentTime = 0f;
 
     // Start is called before the first frame update
     void Awake()
     {
         spawnLocation = GameObject.Find("SpawnLocation");
         Instantiate(playerPrefab, spawnLocation.transform.position, Quaternion.identity);
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // randomly trigger the sheen animation
+        if (!triggerSet)
+        {   
+            triggerTime = Random.Range(5, 10);
+            triggerSet = true;
+        }
+        currentTime += Time.deltaTime;
+        if ((int)(currentTime % 60) == triggerTime) // if seconds line up. play animation (makes it random but frequent)
+        {
+            animator.SetTrigger("sheen");
+            triggerSet = false;
+            currentTime = 0f; // reset time
+        }
+        
     }
 }
